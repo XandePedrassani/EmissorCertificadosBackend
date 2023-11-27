8eb5278ae9393410f06a6f0d9da0bfd43a4ec903
Carlos Eduardo Dal Molin <carlosdudu164@gmail.com>
2023-11-27 19:17:37 -0300

    Pequeno ajuste

--- src/main/java/com/OficinaDeSoftware/EmissorCertificadosBackend/controller/EventoParticipanteController.java
+++ src/main/java/com/OficinaDeSoftware/EmissorCertificadosBackend/controller/EventoParticipanteController.java
@@ -2,7 +2,7 @@ package com.OficinaDeSoftware.EmissorCertificadosBackend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +18,7 @@ public class EventoParticipanteController {
     @Autowired
     private EventoParticipanteService service;
 
-    @GetMapping("/inserir")
+    @PostMapping("/inserir")
     public ResponseEntity<EventoParticipante> insert(@RequestBody EventoParticipanteDto EventoParticipanteDto){
         return ResponseEntity.ok(service.insert(EventoParticipanteDto));
     }
